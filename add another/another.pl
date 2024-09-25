26c103d712922e83ab020a391a4ef47891f8f1e7
Kassahun Addiss <kassahunaddiss6@gmail.com>
2024-09-25 12:02:47 +0300

    cnangh add in the new branch

--- add another/another.pl
+++ add another/another.pl
@@ -37,89 +37,104 @@ div.kv-avatar div.file-input div.file-preview {
 	width: 200px;
 }
 
-/*login form*/
-.vertical {
-	padding-top: 150px;
-	padding-bottom: 150px;
-}
-nav .nav>li>a {
-  color: #fff !important;
-}
-.navbar-nav > li a:hover {
-  background: #00000040 !important;
-}
-.navbar-brand, .navbar-nav > li > a {
-  text-shadow:none !important;
-}
-.navbar-toggle .icon-bar { 
-  background: #fff;
-}
-.dataTables_wrapper { 
-  overflow: auto;
-}
+var manageCategoriesTable;
 
-.navbar-nav.navbar-right {
-  margin-left: 0; /* Set margin-right to 0 */
-  padding-left: 0; /* Set margin-right to 0 */
+$(document).ready(function() {
+	// active top navbar categories
+	$('#navCategories').addClass('active');	
 
-}
+	manageCategoriesTable = $('#manageCategoriesTable').DataTable({
+		'ajax' : 'php_action/fetchCategories.php',
+		'order': []
+	}); // manage categories Data Table
 
-.navbar-nav.navbar-right {
-  margin-right: 5px; /* Set margin-right to 0 */
-  padding-right: 5px; /* Set margin-right to 0 */
+	// on click on submit categories form modal
+	$('#addCategoriesModalBtn').unbind('click').bind('click', function() {
+		// reset the form text
+		$("#submitCategoriesForm")[0].reset();
+		// remove the error text
+		$(".text-danger").remove();
+		// remove the form error
+		$('.form-group').removeClass('has-error').removeClass('has-success');
 
-}
+		// submit categories form function
+		$("#submitCategoriesForm").unbind('submit').bind('submit', function() {
 
-#navDashboard{
+			var categoriesName = $("#categoriesName").val();
+			var categoriesStatus = $("#categoriesStatus").val();
 
-  margin-left: 0; /* Set margin-right to 0 */
-  padding-left: 0; /* Set margin-right to 0 */
-}
+			if(categoriesName == "") {
+				$("#categoriesName").after('<p class="text-danger">Brand Name field is required</p>');
+				$('#categoriesName').closest('.form-group').addClass('has-error');
+			} else {
+				// remov error text field
+				$("#categoriesName").find('.text-danger').remove();
+				// success out for form 
+				$("#categoriesName").closest('.form-group').addClass('has-success');	  	
+			}
+
+			if(categoriesStatus == "") {
+				$("#categoriesStatus").after('<p class="text-danger">Brand Name field is required</p>');
+				$('#categoriesStatus').closest('.form-group').addClass('has-error');
+			} else {
+				// remov error text field
+				$("#categoriesStatus").find('.text-danger').remove();
+				// success out for form 
+				$("#categoriesStatus").closest('.form-group').addClass('has-success');	  	
+			}
+
+			if(categoriesName && categoriesStatus) {
+				var form = $(this);
+				// button loading
+				$("#createCategoriesBtn").button('loading');
+
+				$.ajax({
+					url : form.attr('action'),
+					type: form.attr('method'),
+					data: form.serialize(),
+					dataType: 'json',
+					success:function(response) {
+						// button loading
+						$("#createCategoriesBtn").button('reset');
+
+						if(response.success == true) {
+							// reload the manage member table 
+							manageCategoriesTable.ajax.reload(null, false);						
+
+	  	  			// reset the form text
+							$("#submitCategoriesForm")[0].reset();
+							// remove the error text
+							$(".text-danger").remove();
+							// remove the form error
+							$('.form-group').removeClass('has-error').removeClass('has-success');
+	  	  			
+	  	  			$('#add-categories-messages').html('<div class="alert alert-success">'+
+	            '<button type="button" class="close" data-dismiss="alert">&times;</button>'+
+	            '<strong><i class="glyphicon glyphicon-ok-sign"></i></strong> '+ response.messages +
+		          '</div>');
+
+	  	  			$(".alert-success").delay(500).show(10, function() {
+								$(this).delay(3000).hide(10, function() {
+									$(this).remove();
+								});
+							}); // /.alert
+						}  // if
+
+					} // /success
+				}); // /ajax	
+			} // if
+
+			return false;
+		}); // submit categories form function
+	}); // /on click on submit categories form modal	
+
+}); // /document
 
 
-    /* Full-screen modal */
-    /* .modal-dialog {
-        width: 100%;
-        height: 100%;
-        margin: 0;
-        padding: 0;
-    }
 
-    .modal-content {
-        height: auto;
-        min-height: 100%;
-        border-radius: 0;
-    }
 
-    .modal-body {
-        max-height: 100%;
-        overflow-y: auto;
-    } */
+	return false;
+		}); // submit categories form function
+	}); // /on click on submit categories form modal	
 
-    body {
-      padding: 20px;
-  }
-  .full-page-form {
-      max-width: 900px;
-      margin: auto;
-      padding: 20px;
-      border: 1px solid #ccc;
-      border-radius: 5px;
-      background-color: #fff;
-  }
-  .form-group {
-      margin-bottom: 10px;
-  }
-  .item-row {
-      display: flex;
-      align-items: center;
-      margin-bottom: 10px;
-  }
-  .item-row div {
-      flex: 1;
-      margin-right: 10px;
-  }
-  .item-row div:last-child {
-      margin-right: 0;
-  }
-
+}); // /document
