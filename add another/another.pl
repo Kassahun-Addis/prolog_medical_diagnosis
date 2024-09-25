cca0fbd4b8e36606b6d9c7aedb00353922d894af
Kassahun Addiss <kassahunaddiss6@gmail.com>
2024-09-25 11:51:09 +0300

    logout add in the new branch

---
+++ add another/another.pl
@@ -0,0 +1,125 @@
+nav.navbar:hover {
+	/* box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); */
+}
+
+button.button1:hover {
+	box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
+}
+
+div.card {
+  width: 100%;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  text-align: center;
+}
+
+
+div.cardHeader {
+    background-color: #4CAF50;
+    color: white;
+    padding: 6px;
+    font-size: 40px;
+}
+
+div.cardContainer {
+    padding: 10px;
+}
+
+
+.div-hide {
+	display: none;
+}
+
+.kv-file-zoom {
+	display: none;
+}
+
+div.kv-avatar div.file-input div.file-preview {
+	width: 200px;
+}
+
+/*login form*/
+.vertical {
+	padding-top: 150px;
+	padding-bottom: 150px;
+}
+nav .nav>li>a {
+  color: #fff !important;
+}
+.navbar-nav > li a:hover {
+  background: #00000040 !important;
+}
+.navbar-brand, .navbar-nav > li > a {
+  text-shadow:none !important;
+}
+.navbar-toggle .icon-bar { 
+  background: #fff;
+}
+.dataTables_wrapper { 
+  overflow: auto;
+}
+
+.navbar-nav.navbar-right {
+  margin-left: 0; /* Set margin-right to 0 */
+  padding-left: 0; /* Set margin-right to 0 */
+
+}
+
+.navbar-nav.navbar-right {
+  margin-right: 5px; /* Set margin-right to 0 */
+  padding-right: 5px; /* Set margin-right to 0 */
+
+}
+
+#navDashboard{
+
+  margin-left: 0; /* Set margin-right to 0 */
+  padding-left: 0; /* Set margin-right to 0 */
+}
+
+
+    /* Full-screen modal */
+    /* .modal-dialog {
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+
+    .modal-content {
+        height: auto;
+        min-height: 100%;
+        border-radius: 0;
+    }
+
+    .modal-body {
+        max-height: 100%;
+        overflow-y: auto;
+    } */
+
+    body {
+      padding: 20px;
+  }
+  .full-page-form {
+      max-width: 900px;
+      margin: auto;
+      padding: 20px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      background-color: #fff;
+  }
+  .form-group {
+      margin-bottom: 10px;
+  }
+  .item-row {
+      display: flex;
+      align-items: center;
+      margin-bottom: 10px;
+  }
+  .item-row div {
+      flex: 1;
+      margin-right: 10px;
+  }
+  .item-row div:last-child {
+      margin-right: 0;
+  }
+
